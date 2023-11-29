87c25a187c103165de7c9dfdc0c9dd3a74c9313b
Patrick Ahrend <patrickahrend@yahoo.com>
2023-11-29 12:21:59 +0100

    pre cookie cutter

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 def process_matching_data(excel_file, sheet_name, output_file):
     matching_data = excel_file.parse(sheet_name)
     
+    print(matching_data.columns)
+
     df_labels = matching_data[['requirement_text', '0 = not relevant; 1 = business compliance relevance; 2 = (customer) informative relevance']].copy()
     df_labels = df_labels.rename(columns={
         '0 = not relevant; 1 = business compliance relevance; 2 = (customer) informative relevance': 'label',
@@ -19,8 +21,9 @@ def process_matching_data(excel_file, sheet_name, output_file):
     print(df_labels['label'].value_counts())
 
 
-australinen_excel = pd.ExcelFile("../data/Australia_Use_Cases.xlsx")
+australinen_excel = pd.ExcelFile("../data/Addition_Australia_data_small_use_case_3_hire_employee.xlsx")
 
 
-process_matching_data(australinen_excel, "1_matching_reordered", "../data/1_Matching_Labels.csv")
-process_matching_data(australinen_excel, "2_matching_reordered", "../data/2_Matching_Labels.csv")
+# process_matching_data(australinen_excel, "1_matching_reordered", "../data/1_Matching_Labels.csv")
+# process_matching_data(australinen_excel, "2_matching_reordered", "../data/2_Matching_Labels.csv")
+process_matching_data(australinen_excel, "training_matching", "../data/hiring_employee.csv")
