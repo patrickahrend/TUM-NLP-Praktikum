7d218689b0b061631d9d16bda9c8813314a8486b
Patrick Ahrend <patrickahrend@yahoo.com>
2024-01-06 21:10:32 +0100

    remove nominalNB and add BERT and RNN classifiers

--- src/models/model_classes.py
+++ src/models/model_classes.py
@@ -1,8 +1,11 @@
+import torch
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression, Perceptron, SGDClassifier
-from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
+from sklearn.naive_bayes import GaussianNB, BernoulliNB
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from torch import nn
+from transformers import BertModel
 
 from model_base import ModelBase
 
@@ -10,11 +13,6 @@ from model_base import ModelBase
 class LogisticRegressionModel(ModelBase):
     def __init__(self):
         super().__init__("Logistic_Regression", LogisticRegression(max_iter=1000))
-
-
-class MultinomialNBModel(ModelBase):
-    def __init__(self):
-        super().__init__("MultinomialNB", MultinomialNB())
 
 
 class GaussianNBModel(ModelBase):
@@ -55,3 +53,40 @@ class PerceptronModel(ModelBase):
 class SGDClassifierModel(ModelBase):
     def __init__(self):
         super().__init__("SGDClassifier", SGDClassifier())
+
+
+class BERTForClassification(nn.Module):
+    def __init__(self, num_classes):
+        super(BERTForClassification, self).__init__()
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+        self.fc = nn.Linear(self.bert.config.hidden_size, num_classes)
+
+    def forward(self, input_ids, attention_mask):
+        output = self.bert(input_ids=input_ids, attention_mask=attention_mask)
+        pooled_output = output.pooler_output
+        return self.fc(pooled_output)
+
+
+class RnnTextClassifier(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, num_layers, dropout=0):
+        super(RnnTextClassifier, self).__init__()
+
+        self.rnn = nn.RNN(
+            input_size,
+            hidden_size,
+            num_layers,
+            batch_first=True,
+            dropout=dropout if num_layers > 1 else 0,
+        )
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.rnn.num_layers, x.size(0), self.rnn.hidden_size)
+
+        out, _ = self.rnn(x, h0)
+
+        out = self.dropout(out)
+        out = self.fc(out[:, -1, :])
+
+        return out
